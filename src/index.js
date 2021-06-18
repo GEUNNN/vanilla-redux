10f3350456e4552f1e6027bce7b7ee8eab6eac7c
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-06-18 22:37:41 +0900

    Finished: Pure Redux basics

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-  number.innerText = count;
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+//function that is called with current state of application and action.
+// action is the way I communicate with countModifier.
+// whatever reducer(countModifier) returns that becomes the state of the application
+let countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
+let countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+// user sends actions to reducer by using dispatch
+// actions must have type key in the object
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
